87d3751fd375e4417eab1660ad07207e587d58ce
deepphp <129658186+deepphp@users.noreply.github.com>
2023-09-11 08:56:06 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -174,7 +174,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href={https://chatgpt.yunweb.cf/} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
